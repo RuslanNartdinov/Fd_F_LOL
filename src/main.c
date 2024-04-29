ae446e3ea75d0a03cf6b2da478db424b46c903e4
RuslanNartdinov <nartdinovruslan2020@gmail.com>
2024-04-29 15:21:51 +0400

    1.0.2

--- src/main.c
+++ src/main.c
@@ -2,29 +2,30 @@
 //gcc src/*.c helpers/*.c minilibx_macos/libmlx.a -framework OpenGL -framework AppKit
 //./a.out test_maps/42.fdf   
 
-int deal_key(int key, t_fdf *data){
+int	deal_key(int key, t_fdf *data)
+{
 	printf("%d\n", key);
-	if(key >= 123 && key <= 126)
+	if (key >= 123 && key <= 126)
 		shift_xy(data, key);
-	if(key == 24 || key == 27)
+	if (key == 24 || key == 27)
 		change_z(data, key);
-	if(key == 53){
+	if (key == 53)
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	}
 	mlx_clear_window(data->mlx_ptr, data->win_ptr);
 	draw_map(data);
 	return (0);
 }
 
-int main(int argc, char **argv){
-	t_fdf *data;
-	data = (t_fdf*)malloc(sizeof(t_fdf));
-	read_file(argv[1],data);
+int	main(int argc, char **argv)
+{
+	t_fdf	*data;
+
+	data = (t_fdf *)malloc(sizeof(t_fdf));
+	read_file(argv[1], data);
 	data->mlx_ptr = mlx_init();
 	data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "FDF");
 	data->zoom = 20 + argc - argc;
-
 	draw_map(data);
-	mlx_key_hook(data->win_ptr,deal_key, data);
+	mlx_key_hook(data->win_ptr, deal_key, data);
 	mlx_loop(data->mlx_ptr);
 }
--- src/mutate.c
+++ src/mutate.c
@@ -2,29 +2,31 @@
 
 void	shift_xy(t_fdf *data, int key)
 {
-	if(key == 126)
-		data->shift_y-=10;
-	if(key == 125)
-		data->shift_y+=10;
-	if(key == 123)
-		data->shift_x-=10;
-	if(key == 124)
-		data->shift_x+=10;
+	if (key == 126)
+		data->shift_y -= 10;
+	if (key == 125)
+		data->shift_y += 10;
+	if (key == 123)
+		data->shift_x -= 10;
+	if (key == 124)
+		data->shift_x += 10;
 }
+
 void	change_z(t_fdf *data, int key)
 {
-	int i;
-	int j;
-	int change;
-	if(key == 24)
+	int	i;
+	int	j;
+	int	change;
+
+	if (key == 24)
 		change = 10;
 	else
 		change = -10;
 	i = 0;
-	while(i<data->height)
+	while (i < data->height)
 	{
 		j = 0;
-		while(j<data->width)
+		while (j < data->width)
 		{
 			data->z_matrix[i][j] += change;
 			j++;
